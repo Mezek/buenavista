97ab7e963c5f25006156b35bbc272dfc6f1eab9c
mezekus <mezekus@hg.code.sf.net>
2015-06-12 15:37:46 +0200

    Constants

---
+++ Nucleons/modules/ConstDelta.cpp
@@ -0,0 +1,16 @@
+/**
+ * $Date$
+ * $Revision$
+ * $Author$
+ * $HeadURL: http://triglav/repos/BuenaVista/Nucleons/modules/ConstDelta.cpp $
+ * $Id$
+ *
+ * @file
+ * @brief	Delta constants for numerical calculations.
+ */
+
+	/// Masses and widths are in GeV
+
+	static const double massDi = 1.232;      // Delta(1232)	
+	static const double widthDi = 0.117;
+
